27fe57a19761c2a6c24363ab5e7a272b439724e2
Max <max.farras@debitfinance.co.uk>
2017-02-20 13:01:59 +0000

    Support Dynamic Config

--- src/Dotmailer/Config.php
+++ src/Dotmailer/Config.php
@@ -8,8 +8,13 @@ class Config
 {
     protected $config;
 
-    public function __construct($config_file)
+    public function __construct($config_file = null, $config_data = null)
     {
+        if($config_file === null) {
+            $this->config = $config_data;
+            return;
+        }
+
         $parser = new Parser();
         $this->config = $parser->parse(file_get_contents($config_file));
     }
